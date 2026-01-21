60dc0f1f18dd0dbdcfa02216ff77f63206cfa8a5
Luis Rivera <Luis_Rivera1007@hotmail.com>
2026-01-21 14:56:33 -0600

    reboot

--- franxx/routes/applications.py
+++ franxx/routes/applications.py
@@ -3,10 +3,3 @@ from fastapi import APIRouter
 ## instantiate api router
 router = APIRouter()
 
-@router.post("/{vg_id}/user/{user_id}/documents/presign")
-async def get_vg_document_presigned_url(
-    vg_id: str,
-    user_id: str,
-):
-    try:
-        
