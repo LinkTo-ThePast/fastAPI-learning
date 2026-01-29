ede5d8775527df1112996c754cac34ade992246b
Luis Rivera <Luis_Rivera1007@hotmail.com>
2026-01-29 12:47:07 -0600

    chore(fastapi fundamentals): understanding routing

--- franxx/main.py
+++ franxx/main.py
@@ -1,29 +1,35 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
 
-class VideogameGenre(str, Enum):
-    RPG = "rpg"
-    ACTION = "action"
-    SURVIVAL_HORROR = "survival-horror"
-    
-@app.get("/")
-async def root():
-	return {"message": "Clair Obscure"}
+class VideoGame(BaseModel):
+    id: int
+    title: str
 
-@app.get("/get-videogame/{vg_id}")
-async def getVideogame(vg_id: str):
-    vg = {"name": "Final Fantasy VII: Rebirth",
-          "relesead_year:": 2024}
-    return {"success": True, "data": vg, "vg_id": vg_id}
+videogames_list: List[VideoGame] = [
+    VideoGame(id=1, title="Ninja Gaiden 2 Black"),
+    VideoGame(id=2, title="The Legend of Zelda: Ocarina of Time"),
+    VideoGame(id=3, title="Luigi's Mansion") 
+]
 
-@app.get("/get-videogame-genre/{vg_genre}")
-async def getVideogameGenre(vg_genre: VideogameGenre):
-    if vg_genre == VideogameGenre.RPG:
-        return {"success": True, "genre": VideogameGenre.RPG, "description": "Immersive world and history"}
-    if vg_genre is VideogameGenre.SURVIVAL_HORROR:
-        return {"success": True, "genre": VideogameGenre.SURVIVAL_HORROR.value, "description": "limited resources, dread"}
-    return {"sucess": True, "genre": VideogameGenre.ACTION.value, "description": "fast combos and skills" }
-        
+
+@app.get("/health")
+def health_check():
+    return { "status": "ok" }
+
+
+@app.get("/list-videogames")
+async def get_list_videogames():
+    return videogames_list
+
+
+@app.get("/list-videogames/{id}")
+async def get_videogame_by_id(id: int):
+    for entry in videogames_list:
+        if entry.id == id:
+            return entry
+    return {"success": False, "message": "Please, provide a valid ID!"}
--- franxx/routes/applications.py
+++ franxx/routes/applications.py
@@ -3,3 +3,5 @@ from fastapi import APIRouter
 ## instantiate api router
 router = APIRouter()
 
+
+
